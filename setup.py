2bbe9bcf729340640fc4750b423c09651f7b41d6
devgupta98 <devgupta09082001@gmail.com>
2023-12-31 20:33:35 +0530

    my first comit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='myfirst_webapp',
+    version='',
+    packages=['myfirst_webapp'],
+    url='',
+    license='',
+    author='ajayg',
+    author_email='manage.py@myfirst_webapp',
+    description=''
+)
